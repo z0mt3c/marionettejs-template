2dad61cca6815ff511e856aae38d1f9a2a55c9fa
Timo Behrmann <timo.behrmann@gmail.com>
2013-11-21 10:40:30 +0100

    getting better - missing: activation of side entries through router & event

--- app/scripts/apps/masterdetail/MasterDetailController.js
+++ app/scripts/apps/masterdetail/MasterDetailController.js
@@ -1,4 +1,26 @@
 define(['application', 'apps/masterdetail/MasterDetailView'], function (App, View) {
+    function getSideView() {
+        var defer = $.Deferred();
+
+        require(['entities/masterdetail'], function () {
+            var fetchEntities = App.request('masterdetail:entities');
+
+            $.when(fetchEntities).done(function (entities) {
+                var view = new View.ManufacturerList({
+                    collection: entities
+                });
+
+                entities.on('select:one', function (model) {
+                    App.trigger('masterdetail:detail', model.get('id'));
+                });
+
+                defer.resolve(view);
+            });
+        });
+
+        return defer.promise();
+    }
+
     function getLayout() {
         var defer = $.Deferred();
 
@@ -6,55 +28,42 @@ define(['application', 'apps/masterdetail/MasterDetailView'], function (App, Vie
             defer.resolve(App.mainRegion.currentView);
         } else {
             var layout = new View.Layout();
+            var loadSide = getSideView();
 
-            require(['entities/masterdetail'], function () {
-                var fetchEntities = App.request('masterdetail:entities');
-
-                $.when(fetchEntities).done(function (entities) {
-                    var view = new View.ManufacturerList({
-                        collection: entities
-                    });
-
-                    entities.on('select:one', function (model) {
-                        App.trigger('masterdetail:detail', model.get('id'));
-                    });
-
-                    App.mainRegion.show(layout);
-                    layout.sideRegion.show(view);
-
-                    defer.resolve(layout);
-                });
+            $.when(loadSide).done(function (sideView) {
+                App.mainRegion.show(layout);
+                layout.sideRegion.show(sideView);
+                defer.resolve(layout);
             });
         }
 
         return defer.promise();
     }
 
-    var controller = {};
+    var controller = {
+        showDetail: function (id) {
+            var loadLayout = getLayout();
 
-    controller.showDetail = function (id) {
-        var loadLayout = getLayout();
+            $.when(loadLayout).done(function (layout) {
+                require(['entities/masterdetail'], function () {
+                    var fetchEntity = App.request('masterdetail:entity', id);
+                    $.when(fetchEntity).done(function (entity) {
+                        var detailView = new View.Detail({
+                            model: entity
+                        });
 
-        $.when(loadLayout).done(function (layout) {
-            require(['entities/masterdetail'], function () {
-                var fetchEntity = App.request('masterdetail:entity', id);
-                $.when(fetchEntity).done(function (entity) {
-                    var detailView = new View.Detail({
-                        model: entity
+                        layout.mainRegion.show(detailView);
                     });
-
-                    layout.mainRegion.show(detailView);
                 });
             });
-        });
-    };
+        },
+        showStart: function () {
+            var loadLayout = getLayout();
 
-    controller.showStart = function () {
-        var loadLayout = getLayout();
-
-        $.when(loadLayout).done(function (layout) {
-            layout.mainRegion.show(new View.Empty({}));
-        });
+            $.when(loadLayout).done(function (layout) {
+                layout.mainRegion.show(new View.Empty({}));
+            });
+        }
     };
 
     return controller;
