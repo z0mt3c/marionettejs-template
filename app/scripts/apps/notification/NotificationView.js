60878f68a7f76b322c59b9ea6a715209fd2e9683
Timo Behrmann <timo.behrmann@gmail.com>
2014-05-28 11:22:36 +0200

    cleanup

--- app/scripts/apps/notification/NotificationView.js
+++ app/scripts/apps/notification/NotificationView.js
@@ -1,28 +1,28 @@
 define(['application',
-    'backbone',
-    'hbs!apps/notification/templates/list',
-    'hbs!apps/notification/templates/listItem'],
+        'backbone',
+        'hbs!apps/notification/templates/list',
+        'hbs!apps/notification/templates/listItem'],
     function (App, Backbone, listTpl, listItemTpl) {
         var views = {};
 
         views.Notification = Backbone.Marionette.ItemView.extend({
             template: listItemTpl,
             tagName: 'li',
-            className: 'notification-item animated fadeInRightBig fast',
+            className: 'notification-item list-group-item animated fadeInRightBig fast',
             events: {
             },
-            onRender: function() {
+            onRender: function () {
                 var self = this;
-                this.$el.addClass(this.model.get('type'));
+                this.$el.addClass('list-group-item-' + this.model.get('type'));
                 var timeout = this.model.get('timeout');
 
-                setTimeout(function() {
-                    self.$el.slideUp('fast', function() {
+                setTimeout(function () {
+                    self.$el.slideUp('fast', function () {
                         self.model.destroy();
                     });
                 }, timeout);
 
-                this.$el.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function() {
+                this.$el.one('webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', function () {
                     self.$el.removeClass('animated bounceInRight');
                 });
             }
@@ -30,7 +30,7 @@ define(['application',
 
         views.Notifications = Backbone.Marionette.CompositeView.extend({
             template: listTpl,
-            className: 'notifications',
+            className: 'notifications list-group',
             itemView: views.Notification,
             itemViewContainer: 'ul',
             events: {
